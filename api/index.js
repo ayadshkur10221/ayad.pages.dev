34e33f739237834530170533d33292e1d917bf69
Ayadshkur100221 <ayadshkur10221@gmail.com>
2025-05-22 21:23:00 +0300

    Fix: converted Express app for Vercel using serverless-http

--- api/index.js
+++ api/index.js
@@ -15,7 +15,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 
 // Middleware
-app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(session({
